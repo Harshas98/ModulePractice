a001492d9cfc2296ddf36b3fe8f5e96493575486
root <root@localhost.localdomain>
2021-01-31 08:06:51 +0530

    added calc package

---
+++ github.com/Harshas98/calc/arithmetic.go
@@ -0,0 +1,17 @@
+package main
+
+func Add(num1, num2 float64) float64 {
+	return num1+num2
+}
+
+func Substract(num1, num2 float64) float64 {
+	return num1 - num2
+}
+
+func Multiply(num1, num2 float64) float64 {
+	return num1 * num2
+}
+
+func Divide(num1, num2 float64) float64 {
+	return num1 / num2
+}
